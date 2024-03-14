d8759e90359c8f47fc9c5fca04b323691cea2577
EdwardDiazR <edwardadriandiaz2020@gmail.com>
2024-03-14 12:17:24 -0400

    remov .vs

--- backend/NegocioInversiones/obj/Debug/net8.0/NegocioInversiones.AssemblyInfo.cs
+++ backend/NegocioInversiones/obj/Debug/net8.0/NegocioInversiones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NegocioInversiones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e77d6804bbaf9a3ec42a38aa12fabc343516ce1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NegocioInversiones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NegocioInversiones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
